8f4fb2d29f378aef858bdea372b5ffb1d282f7a3
Jaehyuk Lee <90822899+duram21@users.noreply.github.com>
2024-12-30 01:32:09 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+ll n, a, b, c, m, k, q, d, tc;
+
+void solve() {
+	while (true) {
+		cin >> a >> b;
+		if (!a && !b) break;
+		cout << a + b << '\n';
+	}
+
+}
+
+
+int main(void) {
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	//cin >> tc;
+	tc = 1;
+	while (tc--) {
+		solve();
+	}
+}
