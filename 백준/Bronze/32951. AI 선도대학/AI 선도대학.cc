6db9259caa5f1527223390dd91d9ebef1001be4b
Jaehyuk Lee <90822899+duram21@users.noreply.github.com>
2025-01-01 02:56:48 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+ll n, a, b, c, m, k, q, d, tc;
+
+void solve() {
+	cin >> a;
+	cout << a - 2024 << '\n';
+
+}
+
+
+int main(void) {
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	//cin >> tc;
+	tc = 1;
+	while (tc--) {
+		solve();
+	}
+}
