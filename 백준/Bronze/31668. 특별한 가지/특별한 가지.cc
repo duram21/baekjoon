7418845f51ae13f221b7b52648ca6c240d76a2fd
Jaehyuk Lee <90822899+duram21@users.noreply.github.com>
2025-02-14 17:16:27 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+ll n, q, a, b, c, m1, m2, tc, d, k, m, l, r, x, s, t, w;
+ll arr[1010101];
+
+void solve() {
+	cin >> n >> m >> k;
+	cout << m / n * k << '\n';
+}
+
+
+int main(void) {
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	tc = 1;
+	//cin >> tc;
+	while (tc--) {
+		solve();
+	}
+}
