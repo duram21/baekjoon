59c0a83317ff02dbaf871583eab73e31e4c53c78
Jaehyuk Lee <90822899+duram21@users.noreply.github.com>
2025-03-07 20:44:46 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,7 +1,11 @@
-SET @HOUR = -1;
-SELECT (@HOUR := @HOUR +1) AS HOUR,
-    (SELECT COUNT(HOUR(DATETIME)) 
-    FROM ANIMAL_OUTS 
-    WHERE HOUR(DATETIME)=@HOUR) AS COUNT 
-FROM ANIMAL_OUTS
-WHERE @HOUR < 23;
+with recursive cte as (
+    select 0 as num
+    union all
+    select num + 1 as num
+    from cte
+    where num < 23
+)
+
+select cte.num as HOUR, count(DATETIME) as COUNT 
+from cte left join ANIMAL_OUTS as AO on cte.num = HOUR(AO.DATETIME)
+group by cte.num
