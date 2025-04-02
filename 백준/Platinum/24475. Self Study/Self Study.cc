24daa6ce71a14cc1668990447d9732d02ecde290
Jaehyeok Lee <90822899+duram21@users.noreply.github.com>
2025-04-02 21:52:37 +0900

    [Platinum V] Title: Self Study, Time: 224 ms, Memory: 7544 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/24475. Self Study/Self Study.cc
+++ 백준/Platinum/24475. Self Study/Self Study.cc
@@ -9,22 +9,16 @@ ll arr[101010];
 ll A[303030];
 ll B[303030];
 
-ll func(ll x) {
+bool func(ll x) {
 	ll cnt = 0;
 	for (int i = 1; i <= n; i++) {
 		ll cur = 0;
-		if (A[i] > B[i]) {
-			ll tmp = (x + A[i] - 1) / A[i];
-			cur = min(tmp, m) * A[i];
-			cnt += min(tmp, m);
-		}
-		if (cur >= x) continue;
-		ll rm = x - cur;
-		ll tmp = (rm + B[i] - 1) / B[i];
-		cnt += tmp;
+			if (x <= A[i] * m) cnt += (x + A[i] - 1) / A[i];
+			else cnt += m + (x - A[i] * m + B[i] - 1) / B[i];
+			if (cnt > n * m) return 0;
 	}
-	if (cnt <= n * m) return true;
-	return false;
+	if (cnt > n * m) return 0;
+	return 1;
 }
 
 void solve() {
@@ -38,18 +32,9 @@ void solve() {
 	}
 	ll l = 1;
 	ll r = 2e18;
-	auto check = [&](ll x) -> bool {
-		ll day = 0;
-		for (ll i = 1; i <= n; i++) {
-			if (x <= A[i] * m) day += (x + A[i] - 1) / A[i];
-			else day += m + (x - A[i] * m + B[i] - 1) / B[i];
-			if (day > n * m) return 0;
-		}
-		return 1;
-	};
 	while (l <= r) {
 		ll mid = (l + r) >> 1;
-		if (check(mid)) {
+		if (func(mid)) {
 			l = mid + 1;
 		}
 		else r = mid - 1;
