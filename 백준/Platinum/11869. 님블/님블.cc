8e0385fd7fb5a41df654a474d93ea1071e660780
Jaehyeok Lee <90822899+duram21@users.noreply.github.com>
2025-05-12 21:47:43 +0900

    [Platinum IV] Title: 님블, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Platinum/11869. 님블/님블.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+using ll = long long;
+
+ll n, a, b, c, d, k, m, x, l, h, w, q;
+ll arr[1010];
+
+
+void solve() {
+	cin >> n;
+	ll xr = 0;
+	for (int i = 1; i <= n; i++) {
+		cin >> a;
+		xr ^= a;
+	}
+	if (!xr) {
+		cout << "cubelover\n";
+	}
+	else cout << "koosaga\n";
+}
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	ll tc = 1;
+	//cin >> tc;
+	while (tc--) {
+		solve();
+	}
+}
