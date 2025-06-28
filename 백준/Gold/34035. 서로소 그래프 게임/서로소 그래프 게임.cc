9ee3be0aa74352a5ec8a67a731e7b462e82d0600
Jaehyeok Lee <90822899+duram21@users.noreply.github.com>
2025-06-29 06:34:56 +0900

    [Gold III] Title: 서로소 그래프 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/34035. 서로소 그래프 게임/서로소 그래프 게임.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+using ll = long long;
+
+ll n, a, b, c, d, f, k, p, e, q, m;
+
+void solve() {
+    cin >> n;
+    if(n == 2) cout << "kang\n";
+    else cout << "chan\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    ll tc = 1;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
