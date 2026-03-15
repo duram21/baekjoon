410a90d55896cc66da9d3e7a1e4f505f13a9b8b5
Jaehyeok Lee <90822899+duram21@users.noreply.github.com>
2026-03-16 04:43:17 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void)
 {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false); cin.tie(NULL);
 	int tc, a, b;
 	cin >> tc;
 	while (tc--)
